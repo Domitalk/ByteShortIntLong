d3f0117829c7aa719da0124d49a39be0bd512014
domitalk <dominic.m.chu@gmail.com>
2020-03-17 18:54:25 -0400

    whole number primitives finished

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -57,8 +57,32 @@ public class Main {
         // the L at the end makes the data into a Long value but without it the saved val is int and it has to be lower than the max
         // In this example, the value without L will error bc its int
 
-        
+        int myTotal = (myMinIntValue / 2);
 
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        // casting is req because otherwise java compiler thinks of myMinByteValue as a int within the the parans
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+        // again, casting. so that the datatype used in the expression is specific, otherwise defaults to int
+
+
+        byte exampleByte = 100;
+
+        short exampleShort = 10000;
+
+        int exampleInt = 1000000000;
+
+        long exampleLong = 50000L + (10L * (exampleByte + exampleShort + exampleInt));
+        // because the definition was triggered by long, the numbers designated to it need to be long.
+        // in this case the 50000L and 10L make the data into long type. So don't need to cast it here
+
+        short shortTotal = (short) (1000 + 10 * (
+                exampleByte + exampleShort + exampleInt
+                ));
+        // in this case because there is no modifying L and inside the () all the numbers are calculated as integers,
+        // it needs a casting
+        
+        System.out.println(exampleLong);
 
 
     }
